be11dba0c9b7d8ebd9b7b1d0f8b5396bc752d5c6
wcvanvan <1057736842@qq.com>
2021-12-08 18:47:40 +0800

    update buzzer

--- buzzer.v
+++ buzzer.v
@@ -20,22 +20,25 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module buzzer(input [16:0] code, // 14 * 8 + 7 * 4
+module buzzer(input [139:0] code,     // 14 * 8 + 7 * 4
               input clk,
-              output reg clk2);
+              output reg clk_buzzer);
 // initial begin
 //     clk2 = 1'b0;
 // end
 wire clk_out;
+reg [7:0] switches;
 clk_div_even #(50000000, 26) clk_divider(.clk(clk), .clk_out(clk_2hz));
 // 5 * 10^7 in binary:10111110101111000010000000 (26bits)
-reg [4:0] bit_counter = 0;
+reg [7:0] bit_counter = 0;
 always @(clk_2hz) begin
-    if (bit_counter == 16) begin
-        bit_counter = 0;
+    if (bit_counter%14 == 0) begin
+        if (switches[bit_counter/14] == 0) begin
+            bit_counter <= bit_counter + 14;
+        end
     end
-    clk2        = code[bit_counter] & clk_2hz;
-    bit_counter = bit_counter + 1;
+    clk_buzzer  <= code[bit_counter] & clk_2hz;
+    bit_counter <= bit_counter + 1;
     // $display("clk2hz: %d, bit_counter: %d, code: %d, clk2: %d",clk_2hz, bit_counter, code[bit_counter], clk2);
 end
 endmodule
